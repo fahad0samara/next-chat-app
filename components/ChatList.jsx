319ac6a53d5e6742768f28de17bf4cac651dc69d
fahad <fah8ad@outlook.com>
2024-04-21 14:31:30 +0300

    add

---
+++ components/ChatList.jsx
@@ -0,0 +1,117 @@
+// ChatList.jsx
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import ChatBox from "./ChatBox";
+import Loader from "./Loader";
+import { pusherClient } from "@lib/pusher";
+import { Delete } from "@mui/icons-material";
+
+const ChatList = ({ currentChatId }) => {
+  const { data: session } = useSession();
+  const currentUser = session?.user;
+
+  const [loading, setLoading] = useState(true);
+  const [chats, setChats] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const getChats = async () => {
+    try {
+      const res = await fetch(
+        search !== ""
+          ? `/api/users/${currentUser._id}/searchChat/${search}`
+          : `/api/users/${currentUser._id}`
+      );
+      const data = await res.json();
+      setChats(data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteChat = async (chatId) => {
+    try {
+      const res = await fetch(`/api/chats/${chatId}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        setChats(chats.filter((chat) => chat._id !== chatId));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (currentUser) {
+      getChats();
+    }
+  }, [currentUser, search]);
+
+  useEffect(() => {
+    if (currentUser) {
+      pusherClient.subscribe(currentUser._id);
+
+      const handleChatUpdate = (updatedChat) => {
+        setChats((allChats) =>
+          allChats.map((chat) => {
+            if (chat._id === updatedChat.id) {
+              return { ...chat, messages: updatedChat.messages };
+            } else {
+              return chat;
+            }
+          })
+        );
+      };
+
+      const handleNewChat = (newChat) => {
+        setChats((allChats) => [...allChats, newChat]);
+      };
+
+      pusherClient.bind("update-chat", handleChatUpdate);
+      pusherClient.bind("new-chat", handleNewChat);
+
+      return () => {
+        pusherClient.unsubscribe(currentUser._id);
+        pusherClient.unbind("update-chat", handleChatUpdate);
+        pusherClient.unbind("new-chat", handleNewChat);
+      };
+    }
+  }, [currentUser]);
+
+  return loading ? (
+    <Loader />
+  ) : chats.length === 0 ? null : (
+    <div className="chat-list-container">
+      <input
+        placeholder="Search chat..."
+        className="input-search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <div className="chats">
+        {chats.map((chat, index) => (
+          <div key={chat._id} className="chat-item">
+            <ChatBox
+              chat={chat}
+              index={index}
+              currentUser={currentUser}
+              currentChatId={currentChatId}
+            />
+            <button
+              onClick={() => deleteChat(chat._id)}
+              className="delete-btn"
+            >
+              <Delete />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ChatList;
---
+++ components/TopBar.jsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Logout } from "@mui/icons-material";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const TopBar = () => {
+  const pathname = usePathname();
+
+  const handleLogout = async () => {
+    signOut({ callbackUrl: "/" });
+  };
+
+  const { data: session } = useSession();
+  const user = session?.user;
+
+  return (
+    <div className="topbar">
+      <Link href="/chats">
+        <img src="/assets/go.png" alt="go" className="h-20 w-20"
+    
+        
+
+         />
+      </Link>
+
+      <div className="menu">
+        <Link
+          href="/chats"
+          className={`${
+            pathname === "/chats" ? "text-green-500" : ""
+          } text-heading4-bold`}
+        >
+          Chats
+        </Link>
+        <Link
+          href="/contacts"
+          className={`${
+            pathname === "/contacts" ? "text-green-500" : ""
+          } text-heading4-bold`}
+        >
+          Contacts
+        </Link>
+
+        <Logout
+          sx={{ color: "#737373", cursor: "pointer" }}
+          onClick={handleLogout}
+        />
+
+        <Link href="/profile">
+          <img
+            src={user?.profileImage || "/assets/person.jpg"}
+            alt="profile"
+            className="profilePhoto"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
